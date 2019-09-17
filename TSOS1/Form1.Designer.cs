3f63fe7525043efabab0e57059919aec80eb0a6b
unknown <semenenya.vitaliy@gmail.com>
2019-09-17 21:07:33 +0300

    First commit

---
+++ TSOS1/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace TSOS1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.cbSettingsMode1 = new System.Windows.Forms.ComboBox();
+            this.trackBar7 = new System.Windows.Forms.TrackBar();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.trackBar5 = new System.Windows.Forms.TrackBar();
+            this.label6 = new System.Windows.Forms.Label();
+            this.trackBar6 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1056, 445);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cbSettingsMode1
+            // 
+            this.cbSettingsMode1.FormattingEnabled = true;
+            this.cbSettingsMode1.Location = new System.Drawing.Point(811, 12);
+            this.cbSettingsMode1.Name = "cbSettingsMode1";
+            this.cbSettingsMode1.Size = new System.Drawing.Size(344, 24);
+            this.cbSettingsMode1.TabIndex = 1;
+            // 
+            // trackBar7
+            // 
+            this.trackBar7.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.trackBar7.Location = new System.Drawing.Point(811, 360);
+            this.trackBar7.Maximum = 1000;
+            this.trackBar7.Minimum = 10;
+            this.trackBar7.Name = "trackBar7";
+            this.trackBar7.Size = new System.Drawing.Size(104, 56);
+            this.trackBar7.TabIndex = 10;
+            this.trackBar7.Value = 10;
+            this.trackBar7.ValueChanged += new System.EventHandler(this.trackBar7_ValueChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(808, 419);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 17);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "10";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(808, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 17);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "10";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.trackBar1.Location = new System.Drawing.Point(811, 56);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Minimum = 10;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 56);
+            this.trackBar1.TabIndex = 12;
+            this.trackBar1.Value = 10;
+
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(992, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 17);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "10";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.trackBar2.Location = new System.Drawing.Point(995, 56);
+            this.trackBar2.Maximum = 1000;
+            this.trackBar2.Minimum = 10;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 56);
+            this.trackBar2.TabIndex = 14;
+            this.trackBar2.Value = 10;
+            this.trackBar2.ValueChanged += new System.EventHandler(this.trackBar2_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(808, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 17);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "10";
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.trackBar3.Location = new System.Drawing.Point(811, 144);
+            this.trackBar3.Maximum = 1000;
+            this.trackBar3.Minimum = 10;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(104, 56);
+            this.trackBar3.TabIndex = 16;
+            this.trackBar3.Value = 10;
+            this.trackBar3.ValueChanged += new System.EventHandler(this.trackBar3_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(992, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 17);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "10";
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.trackBar4.Location = new System.Drawing.Point(995, 145);
+            this.trackBar4.Maximum = 1000;
+            this.trackBar4.Minimum = 10;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(104, 56);
+            this.trackBar4.TabIndex = 18;
+            this.trackBar4.Value = 10;
+            this.trackBar4.ValueChanged += new System.EventHandler(this.trackBar4_ValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(808, 304);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 17);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "10";
+            // 
+            // trackBar5
+            // 
+            this.trackBar5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.trackBar5.Location = new System.Drawing.Point(811, 245);
+            this.trackBar5.Maximum = 1000;
+            this.trackBar5.Minimum = 10;
+            this.trackBar5.Name = "trackBar5";
+            this.trackBar5.Size = new System.Drawing.Size(104, 56);
+            this.trackBar5.TabIndex = 20;
+            this.trackBar5.Value = 10;
+            this.trackBar5.ValueChanged += new System.EventHandler(this.trackBar5_ValueChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(992, 304);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 17);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "10";
+            // 
+            // trackBar6
+            // 
+            this.trackBar6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.trackBar6.Location = new System.Drawing.Point(995, 245);
+            this.trackBar6.Maximum = 1000;
+            this.trackBar6.Minimum = 10;
+            this.trackBar6.Name = "trackBar6";
+            this.trackBar6.Size = new System.Drawing.Size(104, 56);
+            this.trackBar6.TabIndex = 22;
+            this.trackBar6.Value = 10;
+            this.trackBar6.ValueChanged += new System.EventHandler(this.trackBar6_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1209, 535);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.trackBar6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.trackBar5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.trackBar7);
+            this.Controls.Add(this.cbSettingsMode1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cbSettingsMode1;
+        private System.Windows.Forms.TrackBar trackBar7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TrackBar trackBar5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TrackBar trackBar6;
+    }
+}
+
---
+++ TSOS1/Form1.cs
@@ -0,0 +1,121 @@
+﻿using PdfSharp.Drawing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace TSOS1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbSettingsMode1.DataSource = new List<string>() {"Амплитуда и частота потоянные", "Начальная фаза и частота потоянные", "Начальная фаза и амплитуда потоянные" };
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            //Graphics g = e.Graphics;
+            //Pen myPen = new Pen(Color.Red);
+            //g.Transform = new System.Drawing.Drawing2D.Matrix(1,0,0,-1,0,this.ClientRectangle.Height);
+            //g.DrawLine(myPen, this.ClientRectangle.Left, this.ClientRectangle.Top, this.ClientRectangle.Right, this.ClientRectangle.Bottom);
+
+
+     
+
+        }
+
+        private  void CreateField()
+        {
+            var serviceX = 300;
+
+
+            ZedGraphControl zedGraph = new ZedGraphControl();
+
+            zedGraph.Location = new System.Drawing.Point(0, 0);
+            zedGraph.Name = "zedGraph";
+            zedGraph.Size = new System.Drawing.Size(this.ClientRectangle.Right-serviceX, this.ClientRectangle.Bottom);
+            this.Controls.Add(zedGraph);
+            CreateGraph(zedGraph);
+        }
+
+            private static void CreateGraph(ZedGraphControl zgc)
+        {
+            GraphPane myPane = zgc.GraphPane;
+            // Set the titles and axis labels
+            myPane.Title.Text = "Axis Cross Demo";
+            myPane.XAxis.Title.Text = "My X Axis";
+            myPane.YAxis.Title.Text = "My Y Axis";
+
+            // Make up some data arrays based on the Sine function
+            double x, y;
+            PointPairList list = new PointPairList();
+            list.Add(new PointPair(10,10));
+            list.Add(new PointPair(100, 100));
+            // Generate a red curve with diamond
+            // symbols, and "Porsche" in the legend
+            LineItem myCurve = myPane.AddCurve("Parabola",
+               list, Color.Black, SymbolType.Default);
+            
+            // Set the Y axis intersect the X axis at an X value of 0.0
+            myPane.YAxis.Cross = 0.0;
+            // Turn off the axis frame and all the opposite side tics
+            myPane.Chart.Border.IsVisible = false;
+            myPane.YAxis.Scale.Min = -100;
+            myPane.YAxis.Scale.Max = 100;
+            myPane.XAxis.Scale.Min = -100;
+            myPane.XAxis.Scale.Max = 100;
+            // Calculate the Axis Scale Ranges
+            zgc.AxisChange();
+            zgc.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CreateField();
+        }
+
+        private void trackBar7_ValueChanged(object sender, EventArgs e)
+        {
+            label7.Text = trackBar7.Value.ToString();
+        }
+
+        private void trackBar2_ValueChanged(object sender, EventArgs e)
+        {
+            label2.Text = trackBar2.Value.ToString();
+        }
+
+
+        private void trackBar3_ValueChanged(object sender, EventArgs e)
+        {
+            
+            label3.Text = trackBar3.Value.ToString();
+        }
+
+        private void trackBar4_ValueChanged(object sender, EventArgs e)
+        {
+            label4.Text = trackBar4.Value.ToString();
+        }
+
+        private void trackBar5_ValueChanged(object sender, EventArgs e)
+        {
+            label5.Text = trackBar5.Value.ToString();
+        }
+
+        private void trackBar6_ValueChanged(object sender, EventArgs e)
+        {
+            label6.Text = trackBar6.Value.ToString();
+        }
+    }
+}
